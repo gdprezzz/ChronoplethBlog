d831ffd76e79fbfe128af13503b994343c93a085
gdprezzz <gdprezzz@gmail.com>
2017-11-21 00:33:25 +0100

    demo controller

---
+++ ChronoplethBlog/Controllers/ArticleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChronoplethBlog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            ViewBag.Bla = "bla"; //équivaut à ViewData["bla"] = "bla";
+            return View();
+        }
+    }
+}
